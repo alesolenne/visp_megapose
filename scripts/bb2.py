f415cc97487d1cafb7ffa602e7345335208c3bbd
alesolenne <alesolenne@gmail.com>
2025-03-21 19:57:27 +0100

    bb3d to bb2d

--- scripts/bb2.py
+++ scripts/bb2.py
@@ -3,6 +3,7 @@ import rospy
 from visp_megapose.msg import BB3D
 import numpy as np
 from pytransform3d.rotations import matrix_from_quaternion
+import cv2
 
 def callback(data):
     rospy.loginfo("Bounding box ottenuta")
@@ -48,33 +49,42 @@ def callback(data):
     p7_c = np.matmul(T,p7)
     p8_c = np.matmul(T,p8)
 
-    f_x = 0.1 
-    f_y = 0.2
-    c_x = 0.1
-    c_y = 0.02
-    
-    u1 = f_x * p1_c[0] / p1_c[2] + c_x
-    u2 = f_x * p2_c[0] / p1_c[2] + c_x
-    u3 = f_x * p3_c[0] / p1_c[2] + c_x
-    u4 = f_x * p4_c[0] / p1_c[2] + c_x
-    u5 = f_x * p5_c[0] / p1_c[2] + c_x
-    u6 = f_x * p6_c[0] / p1_c[2] + c_x
-    u7 = f_x * p7_c[0] / p1_c[2] + c_x
-    u8 = f_x * p8_c[0] / p1_c[2] + c_x
+    r_vec = np.array([0.0, 0.0, 0.0])
+    t_vec = np.array([0.0, 0.0, 0.0])
 
-    v1 = f_y * p1_c[1] / p1_c[2] + c_y
-    v2 = f_y * p2_c[1] / p1_c[2] + c_y
-    v3 = f_y * p3_c[1] / p1_c[2] + c_y
-    v4 = f_y * p4_c[1] / p1_c[2] + c_y
-    v5 = f_y * p5_c[1] / p1_c[2] + c_y
-    v6 = f_y * p6_c[1] / p1_c[2] + c_y
-    v7 = f_y * p7_c[1] / p1_c[2] + c_y
-    v8 = f_y * p8_c[1] / p1_c[2] + c_y
+    coords = np.array([p1_c[0:3], p2_c[0:3], p3_c[0:3], p4_c[0:3], p5_c[0:3], p6_c[0:3], p7_c[0:3], p8_c[0:3],])
 
-    u_min = min(u1, u2, u3, u4, u5 ,u6 ,u7, u8)
-    v_min = min(v1, v2, v3, v4, v5 ,v6 ,v7, v8)
-    u_max = max(u1, u2, u3, u4, u5 ,u6 ,u7, u8)
-    v_max = max(v1, v2, v3, v4, v5 ,v6 ,v7, v8)
+    cam_matrix = np.array([[385.76629638671875, 0, 313.4191589355469], [0, 385.35888671875, 244.85601806640625], [0, 0, 1]])
+    distortion = np.array([-0.05377520993351936, 0.06127079576253891, -0.00161056499928236, 0.0007866412051953375, -0.019219011068344116])
+
+    points_2d = cv2.projectPoints(coords, r_vec, t_vec, cam_matrix, distortion)[0]
+
+    u1_p = points_2d[0][0][0]
+    u2_p = points_2d[1][0][0]
+    u3_p = points_2d[2][0][0]
+    u4_p = points_2d[3][0][0]
+    u5_p = points_2d[4][0][0]
+    u6_p = points_2d[5][0][0]
+    u7_p = points_2d[6][0][0]
+    u8_p = points_2d[7][0][0]
+
+    v1_p = points_2d[0][0][1]
+    v2_p = points_2d[1][0][1]
+    v3_p = points_2d[2][0][1]
+    v4_p = points_2d[3][0][1]
+    v5_p = points_2d[4][0][1]
+    v6_p = points_2d[5][0][1]
+    v7_p = points_2d[6][0][1]
+    v8_p = points_2d[7][0][1]
+
+    u_p_min =  min(u1_p, u2_p, u3_p, u4_p, u5_p ,u6_p ,u7_p, u8_p)
+    v_p_min =  min(v1_p, v2_p, v3_p, v4_p, v5_p ,v6_p ,v7_p, v8_p)
+
+    u_p_max =  max(u1_p, u2_p, u3_p, u4_p, u5_p ,u6_p ,u7_p, u8_p)
+    v_p_max =  max(v1_p, v2_p, v3_p, v4_p, v5_p ,v6_p ,v7_p, v8_p)
+
+    print(v_p_min, u_p_min)
+    print(v_p_max, u_p_max)
 
 def listener():
 
--- scripts/bb3_publisher.py
+++ scripts/bb3_publisher.py
@@ -8,9 +8,16 @@ def talker():
     pub = rospy.Publisher('BB3D', BB3D, queue_size=10)
     rospy.init_node('talker_bb', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    
-    T = np.array([0.1,0.2,0.3,0.4])
-    q = np.array([0,0,0,1])
+                    
+    T = np.array([        -0.07546298950910568,
+        -0.010649887844920158,
+        0.4491899907588959])
+    q = np.array([        -0.29764723297929335,
+        0.24537875302813547,
+        0.5299172092793448,
+        0.7552371439301164])
+
+
 
     bb3 = BB3D()
     bb3.pose.translation.x = T[0]
@@ -20,9 +27,9 @@ def talker():
     bb3.pose.rotation.y = q[1]
     bb3.pose.rotation.z = q[2]
     bb3.pose.rotation.w = q[3]
-    bb3.dim_x = 0.2
-    bb3.dim_y = 0.3
-    bb3.dim_z = 0.4
+    bb3.dim_x = 0.06
+    bb3.dim_y = 0.14
+    bb3.dim_z = 0.07
     
     while not rospy.is_shutdown():
 
